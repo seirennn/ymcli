d4ead99df543b1d938cdaf47bfd019aab0353f15
Seirennn <lansohumtsoe@gmail.com>
2026-09-15 06:36:37 +0530

    Add .gitignore and un-track build artifacts

--- build/_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/flexbox_config.cpp.o.d
+++
@@ -1,3 +0,0 @@
-_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/flexbox_config.cpp.o: \
-  /Users/lansohumtsoe/.gemini/antigravity/scratch/ymcli/build/_deps/ftxui-src/src/ftxui/dom/flexbox_config.cpp \
-  /Users/lansohumtsoe/.gemini/antigravity/scratch/ymcli/build/_deps/ftxui-src/include/ftxui/dom/flexbox_config.hpp
